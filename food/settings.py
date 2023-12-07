c86c2803f2aad30b751f38e622f70baafaccde59
Balogun segun <bsegun618@gmail.com>
2023-12-07 21:54:14 +0100

    edited settings.py

--- food/settings.py
+++ food/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # For Deployment
+MEDIA_ROOT = BASE_DIR/'media' # For Deployment
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
