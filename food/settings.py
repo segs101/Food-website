1f7a207b308f17784d80cb67b1dfcf3f888ca9ba
Balogun segun <bsegun618@gmail.com>
2023-12-07 20:19:25 +0100

    edited vercel.json

--- food/settings.py
+++ food/settings.py
@@ -140,12 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static') # For Deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # For Deployment
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # For Deployment
